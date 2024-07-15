75480a3fdfcdfa5f10ddfcbaf754aa538144b3c9
Alpharis <rahulschauhan50@gmail.com>
2024-07-15 21:18:08 +0530

    fixed about me link

--- src/components/sub/hero-content.tsx
+++ src/components/sub/hero-content.tsx
@@ -19,7 +19,10 @@ export const HeroContent = () => {
       animate="visible"
       className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <div
+        className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start"
+        id="about-me"
+      >
         <motion.div
           variants={slideInFromTop}
           className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]]"
